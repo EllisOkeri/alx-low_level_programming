f9006047e158d48332282a0f3f9686beb9722d05
ellisokeri <ellisokeri.com>
2022-10-16 14:47:08 -0700

    Task 9 Betty Fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,7 +9,8 @@ int main(void)
 {
 	int num;
 
-	for (num = '0'; num <= '9'; num++)
+	for (num = '0'; 
+	num <= '9'; num++)
 
 	{
 	putchar(num);
@@ -17,7 +18,8 @@ int main(void)
 	{
 	break;																		}
 	else
-	{																		putchar(',');																					putchar(' ');
+	{
+		putchar(',');																					putchar(' ');
 
 	}
 	}
