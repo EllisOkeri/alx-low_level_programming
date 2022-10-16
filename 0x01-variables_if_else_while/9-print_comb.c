3e9de627d3dfbfca7a1dcd757cd903ed20e6af82
ellisokeri <ellisokeri.com>
2022-10-16 14:52:33 -0700

    Task 9 Formatt Fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,14 @@ int main(void)
 {
 	int num;
 
-	for (num = '0'; 
+	for (num = '0';
 	num <= '9'; num++)
 	{
 	putchar(num);
 	if (num == '9')
 	{
-	break;																		}
+	break;
+	}
 	else
 	{
 	putchar(',');
