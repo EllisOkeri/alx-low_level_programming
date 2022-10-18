ef41dd6a2d10d3af15098abb015b389c99838638
ellisokeri <ellisokeri.com>
2022-10-18 09:25:18 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *  * print_alphabet - prints all alphabets in lowercase.
- *  * Return: On success 1.
+ *  * Return: 0 On success 
  *  */
 void print_alphabet(void)
 {
@@ -11,4 +11,5 @@ void print_alphabet(void)
 	_putchar(i);
 	}
 	_putchar('\n');	 
+	return (0);
 }
