57a43de9ea94aba3b516f8fc84c0caaf7e4c4ec9
ellisokeri <ellisokeri.com>
2022-10-25 16:27:04 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strcpy - copies string pointed by source
+ *with terminating null byte to
+ *buffer pointed to by destination
+ *dest: destination.
+ *@src: source.
+ *Return: pointer to destination
+ **/
+
+char *_strcpy(char *dest, char *src)
+{
+	int num = 0;
+
+	while (num >= 0)
+	{
+		*(dest + num) = *(src + num);
+		if (*(src + num) == '\0')
+			break;
+		num++;
+	}
+	return (dest);
+}
