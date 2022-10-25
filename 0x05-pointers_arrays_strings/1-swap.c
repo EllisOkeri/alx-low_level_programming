26e743241ff660656498948350159785fbe84960
ellisokeri <ellisokeri.com>
2022-10-25 14:24:42 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+include "main.h"
+
+/**
+ *swap_int-Swaps the values of 2 integers.
+ *@a:First value to be swapped.
+ *@b:second value to be swapped.
+ *Return:void.
+**/
+
+void swap_int(int *a, int *b)
+{
+	int *c = *a;
+	*a = *b;
+	*b = *c;
+}
