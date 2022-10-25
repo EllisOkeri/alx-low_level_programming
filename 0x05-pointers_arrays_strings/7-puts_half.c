00147868ab9077964c52328f8bc9c22db7404e36
ellisokeri <ellisokeri.com>
2022-10-25 16:06:51 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *puts_half - prints half of a string
+ *@str: input string.
+ *Return: void.
+**/
+
+void puts_half(char *str)
+{
+	int num = 0, a;
+
+	while (num >= 0)
+	{
+		if (str[num] == '\0')
+			_putchar('\n');
+			break;
+			num++;
+	}
+		if (num % 2 == 1)
+			a = num % 2;
+		else
+			a = (num - 1) / 2;
+		for (a++, a < num, a++)
+			_putchar(str[a]);
+		_putchar('\n');
+}
