6118e66cbcad5a6404772e739331a7a545fa339f
ellisokeri <ellisokeri.com>
2022-10-27 23:23:29 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,13 +4,13 @@
  * _strncat - concatenates two strings,
  * @dest:destination.
  * @src:source.
- * @n: amount of bytes used from src.
+ * @n: amount of bytes used.
  * Return: dest.
 **/
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i,
+	int i;
 	int j;
 
 	i = 0;
