7d3599e6008e090e2c5d55224d75731c345f16fa
ellisokeri <ellisokeri.com>
2022-10-28 01:38:53 -0700

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,17 +10,17 @@ char *leet(char *s)
 {
 	int count = 0, i;
 
-	int low_letters[] = {97, 101, 111, 116, 108};
-	int upp_letters[] = {65, 69, 79, 84, 76};
-	int numbers[] = {52, 51, 48, 55, 49};
+	int small[] = {97, 101, 111, 116, 108};
+	int capital[] = {65, 69, 79, 84, 76};
+	int number[] = {52, 51, 48, 55, 49};
 
 	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 5; i++)
 		{
-			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+			if (*(s + count) == small[i] || *(s + count) == capital[i])
 			{
-				*(s + count) = numbers[i];
+				*(s + count) = number[i];
 				break;
 			}
 		}
