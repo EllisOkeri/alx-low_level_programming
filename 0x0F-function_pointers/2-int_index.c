91e54c5e1cd08b90944345d6b0b25beebfbd97e6
ellisokeri <ellisokeri@gmail.com>
2022-11-17 12:09:22 -0800

    All

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - search an integer
+ * @array: array
+ * @size: size
+ * @cmp: pointer for comparison
+ * Return: no match or size <= 0, return -1 otherwise 0
+**/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
+	}
+	return (-1);
+}
