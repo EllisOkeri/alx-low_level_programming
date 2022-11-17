ccd4eb6131b5a553940fe11446fb219302276b00
ellisokeri <ellisokeri@gmail.com>
2022-11-17 12:27:19 -0800

    Fixing

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name - print character
+ * print_name - prints name
  * @name: name
- * @f: pointer
- * Return: void
+ * @f: function
 **/
 
 void print_name(char *name, void (*f)(char *))
-
 {
 	if (name == NULL || f == NULL)
 		return;
-		f(name);
+	f(name);
 }
